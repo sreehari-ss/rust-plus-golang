d7c849747b624ef60c42f3ed37b20fe899861527
sreehari <sreehari.prasad@suprsend.com>
2025-03-26 11:08:44 +0530

    global vars and error handling

--- lib/hello/src/lib.rs
+++ lib/hello/src/lib.rs
@@ -1,44 +1,114 @@
 extern crate handlebars;
 extern crate serde_json;
-use handlebars::{handlebars_helper, Handlebars};
-use serde_json::Value;
+use core::{error, fmt, str};
+use handlebars::{Handlebars, RenderError, handlebars_helper};
+use lazy_static::lazy_static;
+use serde_json::{Error as SerdeJsonError, Value};
 use std::ffi::c_char;
+use std::str::Utf8Error;
 use std::{
-    ffi::{CStr, CString},
+    ffi::{CStr, CString, NulError},
     str::FromStr,
 };
 
+lazy_static! {
+    pub static ref reg: Handlebars<'static> = {
+        let mut hb = Handlebars::new();
+        hb.register_helper("isdefined", Box::new(isdefined));
+        hb
+    };
+}
 
 handlebars_helper!(isdefined: |v: Value| !v.is_null());
 
+#[derive(Debug)]
+enum HBError {
+    Utf8Error(str::Utf8Error),
+    SerdeJsonError(SerdeJsonError),
+    NulError(NulError),
+    RenderError(RenderError),
+    Other,
+}
+
+impl fmt::Display for HBError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl error::Error for HBError {}
+
+impl From<str::Utf8Error> for HBError {
+    fn from(value: str::Utf8Error) -> Self {
+        HBError::Utf8Error(value)
+    }
+}
+
+impl From<SerdeJsonError> for HBError {
+    fn from(value: SerdeJsonError) -> Self {
+        HBError::SerdeJsonError(value)
+    }
+}
+
+impl From<NulError> for HBError {
+    fn from(value: NulError) -> Self {
+        HBError::NulError(value)
+    }
+}
+
+impl From<RenderError> for HBError {
+    fn from(value: RenderError) -> Self {
+        HBError::RenderError(value)
+    }
+}
+
+struct FFIOut {
+    raw_ptr: *mut c_char,
+}
+
+impl FFIOut {
+    fn new(raw_ptr: *mut c_char) -> Self {
+        FFIOut { raw_ptr }
+    }
+}
+
+impl From<Result<String, HBError>> for FFIOut {
+    fn from(value: Result<String, HBError>) -> Self {
+        let res: Result<CString, HBError> =
+            value.and_then(|value| CString::from_str(&value).map_err(|err| err.into()));
+        match res {
+            Ok(cstr) => Self::new(cstr.into_raw()),
+            Err(err) => {
+                // it's okay to unwrap in here hoping error string wouldn't contain a null characters
+                let err_string = CString::from_str(&err.to_string()).unwrap();
+                FFIOut::new(err_string.into_raw())
+            }
+        }
+    }
+}
 
 #[unsafe(no_mangle)]
-pub extern "C" fn render_template(template: *const c_char,data: *const c_char) -> *mut c_char {
+pub extern "C" fn render_template(template: *const c_char, data: *const c_char) -> *mut c_char {
     let template = unsafe { CStr::from_ptr(template) };
-    let template = template.to_str().unwrap();
-   
+
     let data = unsafe { CStr::from_ptr(data) };
-    let data = data.to_str().unwrap();
 
-    let mut reg = Handlebars::new();
+    let res = render_template_inner(template, data);
+    FFIOut::from(res).raw_ptr
+}
 
-    reg.register_helper("isdefined", Box::new(isdefined));
-
-     reg.register_template_string("tpl_1", template)
-     .unwrap();
-
-    let json:Value = serde_json::from_str(data).unwrap();
-    let ret = reg.render("tpl_1", &json).unwrap();
-
-    CString::from_str(&ret).unwrap().into_raw()
+fn render_template_inner(template: &CStr, data: &CStr) -> Result<String, HBError> {
+    let template = template.to_str()?;
+    let data = data.to_str()?;
+    let json: Value = serde_json::from_str(data)?;
+    reg.render_template(template, &json)
+        .map_err(|err| err.into())
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn free_rust_string(name: *mut c_char)  {
-    unsafe { 
-        drop(CString::from_raw(name)) };
+pub extern "C" fn free_rust_string(name: *mut c_char) {
+    unsafe { drop(CString::from_raw(name)) };
 }
-
 
 // This is present so it's easy to test that the code works natively in Rust via `cargo test`
 #[cfg(test)]
@@ -48,7 +118,11 @@ pub mod test {
 
     #[test]
     fn test_render_template() {
-        render_template(CString::new("Good afternoon, {{name}}, isDefined {{ isdefined age}}").unwrap().into_raw(),CString::new(r#"{"name":"hari"}"#).unwrap().into_raw());
+        render_template(
+            CString::new("Good afternoon, {{name}}, isDefined {{ isdefined age}}")
+                .unwrap()
+                .into_raw(),
+            CString::new(r#"{"name":"hari"}"#).unwrap().into_raw(),
+        );
     }
 }
-
--- main_dynamic.go
+++ main_dynamic.go
@@ -8,13 +8,41 @@ package main
 #include <stdlib.h>
 */
 import "C"
-import "unsafe"
+import (
+	"fmt"
+	"time"
+	"unsafe"
+)
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
 
 func main() {
+	// var wg sync.WaitGroup
+	// defer timer("main")()
+	// for i := 1; i <= 50000; i++ {
+	// 	wg.Add(1)
+	// 	go func() {
+	// 		defer wg.Done()
+	callrust()
+	//		}()
+	//	}
+	//
+	// wg.Wait()
+}
+
+func callrust() {
+
 	template := C.CString("Good afternoon, {{name}}, isDefined {{ isdefined age}}")
-	data := C.CString("{\"name\":\"harweri\"}")
+	// print("hi")
+	data := C.CString("{\"name\":\"hari\"}")
 	defer C.free(unsafe.Pointer(template))
 	defer C.free(unsafe.Pointer(data))
+
 	rString := C.render_template(template, data)
 	// we need to free it this way because of this https://doc.rust-lang.org/std/ffi/struct.CString.html#method.into_raw
 	defer C.free_rust_string(rString)
